34817d7c3a82f448a766796c2cf4f42068b407a3
Claudia Londono <claudia@londonos.com>
2022-01-24 12:03:59 -0600

    commit

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
 const express = require('express')
+const methodOverride = require('method-override')
 const app = express ()
 const PORT = 8000
 
 const mongoose = require('mongoose')
 const URI = 'mongodb://127.0.0.1:27017/recipes'
-
+app.set('view engine','ejs')
 mongoose.connect(URI, ()=>{
     console.log('mongoose connectedat ' + URI)
 })
+//middleware
+app.use(methodOverride('_method'))
+app.use(express.urlencoded({extended:false}))
+
 
 
 
 
 app.listen(PORT, ()=>{
-    console.log('Listening')
+    console.log('Listening',PORT)
 })
 
