5379f163470cd09c5fc89ec13148e3ee404e34cb
Sam-Aum <psaum01@gmail.com>
2022-01-30 14:29:58 -0500

    pic changes

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -104,8 +104,7 @@ router.post('/', (req, res) => {
 
 // edit route
 router.get('/:id/edit', (req, res) => {
-    Recipes.findById(req.params.id).populate('category').exec(
-        (err, foundRecipe) => {
+    Recipes.findById(req.params.id), (err, foundRecipe) => {
         if (err) {
             return res.send(err)
         } 
@@ -116,7 +115,7 @@ router.get('/:id/edit', (req, res) => {
                 {recipe: foundRecipe, category: foundCategory, id: req.params.id })
             })
         }
-    })
+    }
 })
 
 // Delete Route
